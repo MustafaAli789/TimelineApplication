f158e5ae874d477f7d9b4a31f2c6741cceb42cd0
Mustafa Ali <alimustafa0987@gmail.com>
2018-05-23 19:47:35 -0400

    Initial Commit

---
+++ src/TimelineApplication/JavaApplication2.java
@@ -0,0 +1,9 @@
+package TimelineApplication;
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        Start startscreen = new Start();
+        startscreen.setVisible(true);
+        
+    }
+    
+}
