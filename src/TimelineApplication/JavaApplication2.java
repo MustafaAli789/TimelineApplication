f71600515139fe9e7de4032b2f3814843d7172f7
Ali <aliaftab9@hotmail.com>
2018-05-23 20:01:31 -0400

    InitialCommitByAli

--- src/TimelineApplication/JavaApplication2.java
+++ src/TimelineApplication/JavaApplication2.java
@@ -3,7 +3,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         Start startscreen = new Start();
         startscreen.setVisible(true);
-        
+        System.out.println("yeah buddy");
     }
     
 }
