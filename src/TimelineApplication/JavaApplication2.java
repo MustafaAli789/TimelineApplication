a984e1515d3a0191f2b37128cfd16ddf05fe23f1
Ali <aliaftab9@hotmail.com>
2018-05-24 12:39:47 -0400

    Remove commits

--- src/TimelineApplication/JavaApplication2.java
+++ src/TimelineApplication/JavaApplication2.java
@@ -3,8 +3,5 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         Start startscreen = new Start();
         startscreen.setVisible(true);
-        System.out.println("yeah buddy");
-        System.out.println("Shawn is cool");
     }
-    
 }
